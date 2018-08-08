6a3b14e1470954a9aed4cbe9a9c90fd1fb0fd24d
Dilip <dilip.geevarghese@cabotsolutions.com>
2018-08-08 09:09:48 +0530

    Added additional styles

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -16,7 +16,9 @@ namespace GigHub.Controllers
         }
         public ActionResult Index()
         {
-            var upcomingGigs = _context.Gigs.Include(g => g.Artist)
+            var upcomingGigs = _context.Gigs
+                                .Include(g => g.Artist) //For eager loading
+                                .Include(g => g.Genre) //For eager loading
                                 .Where(g => g.DateTime > DateTime.Now);
             return View(upcomingGigs);
         }
