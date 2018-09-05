b84aecca0733658277f7d1c8d8af47a25bbd4586
Dilip <dilip.geevarghese@cabotsolutions.com>
2018-09-05 18:25:12 +0530

    Displayed buton only to logged-in users

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using GigHub.Models;
+using GigHub.ViewModels;
 using System;
 using System.Data.Entity;
 using System.Linq;
@@ -20,7 +21,14 @@ namespace GigHub.Controllers
                                 .Include(g => g.Artist) //For eager loading
                                 .Include(g => g.Genre) //For eager loading
                                 .Where(g => g.DateTime > DateTime.Now);
-            return View(upcomingGigs);
+
+            HomeViewModel viewModel = new HomeViewModel()
+            {
+                UpcomingGigs = upcomingGigs,
+                ShowActions = User.Identity.IsAuthenticated
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult About()
---
+++ GigHub/GigHub/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<GigHub.Models.Gig> UpcomingGigs { get; set; }
+
+        public bool ShowActions { get; set; }
+    }
+}
